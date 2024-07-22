7d53e2cdeb5bbed57e986dab2f17736670937070
MYMEILE <529266369@qq.com>
2024-07-22 14:06:32 +0800

    update session

--- app/utils/group_users.py
+++ app/utils/group_users.py
@@ -1,27 +1,27 @@
-# from telethon.sync import TelegramClient
-# from telethon.tl.types import InputPeerEmpty
+from telethon.sync import TelegramClient
+from telethon.tl.types import InputPeerEmpty
+
+# 填入你的 Telegram API credentials
+api_id = 20464789
+api_hash = "87c3a2090b3c3fd98ea22da5e4d39a44"
+
 #
-# # 填入你的 Telegram API credentials
-# api_id = 20464789
-# api_hash = "87c3a2090b3c3fd98ea22da5e4d39a44"
-#
-#
-# async def main():
-#     async with TelegramClient('session', api_id, api_hash) as client:
-#         # 获取所有 chat
-#         async for dialog in client.iter_dialogs():
-#             # print(f'Chat ID: {dialog.id}, Chat Name: {dialog.name}')
-#             try:
-#                 print(dialog)
-#                 print(dialog.entiry.id)
-#             except Exception as e:
-#                 print(e)
-#
-#
-# if __name__ == '__main__':
-#     import asyncio
-#
-#     asyncio.run(main())
+async def main():
+    async with TelegramClient('session', api_id, api_hash) as client:
+        # 获取所有 chat
+        async for dialog in client.iter_dialogs():
+            # print(f'Chat ID: {dialog.id}, Chat Name: {dialog.name}')
+            try:
+                print(dialog)
+                print(dialog.entiry.id)
+            except Exception as e:
+                print(e)
+
+
+if __name__ == '__main__':
+    import asyncio
+
+    asyncio.run(main())
 
 
 
