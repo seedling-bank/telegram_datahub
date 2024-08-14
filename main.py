12c6505f469af69ffe037041d6824f127033681c
MYMEILE <529266369@qq.com>
2024-08-14 11:49:08 +0800

    update

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ api_hash = '87c3a2090b3c3fd98ea22da5e4d39a44'  # 你的 api_hash
 chat_id = 1002080008623  # 群组ID
 # chat_id = 4140777618  # 群组ID
 
-client = TelegramClient('aaa.session', api_id, api_hash)
+client = TelegramClient('session.session', api_id, api_hash)
 
 engine = create_async_engine(
     "mysql+aiomysql://cb:cryptoBricks123@cb-rds.cw5tnk9dgstt.us-west-2.rds.amazonaws.com/da_test?charset=utf8mb4",
