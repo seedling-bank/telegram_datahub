0cc9b89db098a435be8770ba649f4ab512031249
MYMEILE <529266369@qq.com>
2024-08-14 11:58:20 +0800

    add

--- main.py
+++ main.py
@@ -24,6 +24,11 @@ chat_id = 1002080008623  # 群组ID
 
 client = TelegramClient('session.session', api_id, api_hash)
 
+with client:
+    client.start()  # 启动客户端
+    loguru.logger.info("Telegram客户端连接成功！")  # 添加连接成功的日志消息
+    client.run_until_disconnected()  # 运行客户端直到断开连接
+
 engine = create_async_engine(
     "mysql+aiomysql://cb:cryptoBricks123@cb-rds.cw5tnk9dgstt.us-west-2.rds.amazonaws.com/da_test?charset=utf8mb4",
     pool_pre_ping=True,
