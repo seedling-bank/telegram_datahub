aa3d50cc80e1d7682252b8f1821453f9ef7362ad
MYMEILE <529266369@qq.com>
2024-10-21 21:21:08 +0800

    update alpha x

--- app/models/users_models.py
+++ app/models/users_models.py
@@ -6,111 +6,13 @@ Base = declarative_base()
 
 metadata = MetaData()
 
-t_tg_users = Table(
-    "tg_users",
+t_alphax_tg_user = Table(
+    "alphax_tg_user",
     metadata,
     Column("id", INTEGER(), primary_key=True, autoincrement=True),
-    Column("tg_id", BIGINT()),
-    Column("tg_first_name", String(255, "utf8mb4_unicode_520_ci")),
-    Column("tg_last_name", String(255, "utf8mb4_unicode_520_ci")),
-    Column("tg_username", String(255, "utf8mb4_unicode_520_ci")),
+    Column("tg_id", String(255, "utf8mb4_unicode_520_ci")),
+    Column("tg_name", String(255, "utf8mb4_unicode_520_ci")),
     Column("tg_phone", String(255, "utf8mb4_unicode_520_ci")),
-    Column("create_time", String(255, "utf8mb4_unicode_520_ci")),
-    Column("update_time", String(255, "utf8mb4_unicode_520_ci")),
-    Column("time_at", DATETIME()),
-)
-
-
-t_lumoz_tg_users = Table(
-    "lumoz_tg_users",
-    metadata,
-    Column("id", INTEGER(), primary_key=True, autoincrement=True),
-    Column("tg_id", INTEGER()),
-    Column("user_id", INTEGER()),
-    Column("tg_first_name", String(255, "utf8mb4_unicode_520_ci")),
-    Column("tg_last_name", String(255, "utf8mb4_unicode_520_ci")),
-    Column("tg_username", String(255, "utf8mb4_unicode_520_ci")),
-    Column("tg_phone", String(255, "utf8mb4_unicode_520_ci")),
-    Column("create_time", String(255, "utf8mb4_unicode_520_ci")),
-    Column("update_time", String(255, "utf8mb4_unicode_520_ci")),
-    Column("chain", String(255, "utf8mb4_unicode_520_ci")),
-    Column("tg_code", INTEGER()),
-    Column("time_at", DATETIME()),
-)
-
-t_lumoz_tg_users_info = Table(
-    "lumoz_tg_users_info",
-    metadata,
-    Column("id", INTEGER(), primary_key=True, autoincrement=True),
-    Column("tg_id", BIGINT()),
-    Column("tg_first_name", String(255, "utf8mb4_unicode_520_ci")),
-    Column("tg_last_name", String(255, "utf8mb4_unicode_520_ci")),
-    Column("tg_username", String(255, "utf8mb4_unicode_520_ci")),
-    Column("tg_phone", String(255, "utf8mb4_unicode_520_ci")),
-    Column("create_time", String(255, "utf8mb4_unicode_520_ci")),
-    Column("update_time", String(255, "utf8mb4_unicode_520_ci")),
-    Column("time_at", DATETIME()),
-)
-
-t_users = Table(
-    "users",
-    metadata,
-    Column("id", BIGINT(), primary_key=True),
-    Column("username", Text(4, "utf8mb4_unicode_ci")),
-    Column("avatar", Text(100, "utf8mb4_unicode_ci")),
-    Column("create_time", BIGINT(20)),
-    Column("discord_id", VARCHAR(255, "utf8mb4_unicode_ci")),
-    Column("twitter_id", VARCHAR(255, "utf8mb4_unicode_ci")),
-    Column("twitter_token", VARCHAR(512, "utf8mb4_unicode_ci")),
-    Column("twitter_name", VARCHAR(512, "utf8mb4_unicode_ci")),
-    Column("twitter_username", VARCHAR(512, "utf8mb4_unicode_ci")),
-    Column("discord_name", VARCHAR(255, "utf8mb4_unicode_ci")),
-    Column("country", VARCHAR(255, "utf8mb4_unicode_ci")),
-    Column("email", VARCHAR(255, "utf8mb4_unicode_ci")),
-    Column("verified", VARCHAR(255, "utf8mb4_unicode_ci")),
-    Column("tg_id", VARCHAR(255, "utf8mb4_unicode_ci")),
-    Column("tg_username", VARCHAR(255, "utf8mb4_unicode_ci")),
-    Column("address", VARCHAR(255, "utf8mb4_unicode_ci")),
-    Column("platform", VARCHAR(255, "utf8mb4_unicode_ci")),
-    Column("selected_icon", VARCHAR(255, "utf8mb4_unicode_ci")),
-    Column("chain", VARCHAR(255, "utf8mb4_unicode_ci")),
-    Column("discord_code", VARCHAR(255, "utf8mb4_unicode_ci")),
-    Column("tg_code", VARCHAR(255, "utf8mb4_unicode_ci")),
-)
-
-B2_tg_info = Table(
-    "B2_tg_info",
-    metadata,
-    metadata,
-    Column("id", INTEGER(), primary_key=True, autoincrement=True),
-    Column("tg_id", BIGINT()),
-    Column("tg_first_name", String(255, "utf8mb4_unicode_520_ci")),
-    Column("tg_last_name", String(255, "utf8mb4_unicode_520_ci")),
-    Column("tg_username", String(255, "utf8mb4_unicode_520_ci")),
-    Column("tg_phone", String(255, "utf8mb4_unicode_520_ci")),
-    Column("create_time", String(255, "utf8mb4_unicode_520_ci")),
-    Column("update_time", String(255, "utf8mb4_unicode_520_ci")),
-    Column("time_at", DATETIME()),
-)
-
-t_B2_user = Table(
-    "B2_user",
-    metadata,
-    Column("id", BIGINT(), primary_key=True),
-    Column("user_id", BIGINT()),
-    Column("discord_id", VARCHAR(255, "utf8mb4_unicode_ci")),
-    Column("discord_name", VARCHAR(255, "utf8mb4_unicode_ci")),
-    Column("tg_id", VARCHAR(255, "utf8mb4_unicode_ci")),
-    Column("tg_username", VARCHAR(255, "utf8mb4_unicode_ci")),
-    Column("twitter_id", BIGINT()),
-    Column("twitter_name", VARCHAR(255, "utf8mb4_unicode_ci")),
-    Column("twitter_username", VARCHAR(255, "utf8mb4_unicode_ci")),
-    Column("discord_code", BIGINT()),
-    Column("twitter_code", BIGINT()),
-    Column("tg_code", BIGINT()),
-    Column("task_discord_code", BIGINT()),
-    Column("task_tg_code", BIGINT()),
-    Column("create_time", VARCHAR(255, "utf8mb4_unicode_ci")),
-    Column("update_time", VARCHAR(255, "utf8mb4_unicode_ci")),
     Column("create_at", DATETIME()),
+    Column("update_at", DATETIME())
 )
--- main.py
+++ main.py
@@ -1,35 +1,27 @@
 import asyncio
-import json
 import traceback
-from datetime import datetime, timezone
+from datetime import datetime
 
 import loguru
 import pytz
-import redis
-from sqlalchemy import insert, event, select, update
+
+from sqlalchemy import insert, event
 from sqlalchemy.exc import DisconnectionError
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker
-from redis import asyncio as aioredis
 
 from telethon import TelegramClient, events
 
-from app.models.users_models import t_tg_users, t_lumoz_tg_users_info, t_lumoz_tg_users, t_users, t_B2_user,  B2_tg_info
+from app.models.users_models import t_alphax_tg_user
 from app.utils.send_lark_message import send_a_message
 
 api_id = 20464789  # 你的 api_id
 api_hash = '87c3a2090b3c3fd98ea22da5e4d39a44'  # 你的 api_hash
 
 client = TelegramClient('session', api_id, api_hash)
-# client = TelegramClient('abcd', api_id, api_hash)
-
-# with client:
-#     client.start()  # 启动客户端
-#     loguru.logger.info("Telegram客户端连接成功！")  # 添加连接成功的日志消息
-#     client.run_until_disconnected()  # 运行客户端直到断开连接
 
 engine = create_async_engine(
-    "mysql+aiomysql://cb:cryptoBricks123@cb-rds.cw5tnk9dgstt.us-west-2.rds.amazonaws.com/da_test?charset=utf8mb4",
+    "mysql+aiomysql://deagent:deagentai123@cb-rds.cw5tnk9dgstt.us-west-2.rds.amazonaws.com/alphax_test?charset=utf8mb4",
     pool_pre_ping=True,
     pool_recycle=3600,
     pool_size=10,
@@ -51,159 +43,29 @@ async_session = sessionmaker(
 )
 
 
-# LUMOZ telegram
-@client.on(events.ChatAction(chats=1002210488432))
+# Alpha X telegram
+@client.on(events.ChatAction(chats=1002228316303))
 async def handler(event):
     try:
-        # REDIS_URL = "redis://10.244.4.140:6379"
-        # REDIS_URL = "redis://10.244.4.58:6379"
-        # pool = aioredis.ConnectionPool.from_url(REDIS_URL, max_connections=10000)
-        # redis_client = aioredis.Redis(connection_pool=pool)
-
         utc_time = datetime.utcnow().replace(tzinfo=pytz.utc)
         formatted_utc_time = utc_time.strftime('%Y-%m-%d %H:%M:%S')
-        timestamp = int(utc_time.timestamp() * 1000)
-
-        if event.user_joined or event.user_added:
-            user = await event.get_user()
-
-            if user:
-                loguru.logger.info(
-                    f'新成员加入LUMOZ: {user.id} - {user.first_name} {user.last_name if user.last_name else ""} ')
-
-                information = {
-                    "tg_id": user.id,
-                    "tg_first_name": user.first_name,
-                    "tg_last_name": user.last_name,
-                    "tg_username": user.username,
-                    "tg_phone": user.phone,
-                    "create_time": timestamp,
-                    "update_time": timestamp,
-                    "time_at": formatted_utc_time
-                }
-
-                try:
-                    async with async_session() as session:
-
-                        user_data = {
-                            "tg_code": 1
-                        }
-                        query1 = (
-                            update(t_lumoz_tg_users)
-                            .where(t_lumoz_tg_users.c.tg_id == int(user.id))
-                            .values(**user_data)
-                        )
-                        await session.execute(query1)
-                        await session.commit()
-
-                        query = insert(t_lumoz_tg_users_info).prefix_with("IGNORE").values(information)
-                        await session.execute(query)
-                        await session.commit()
-                except Exception as e:
-                    loguru.logger.error(traceback.format_exc())
-                    send_a_message(traceback.format_exc())
-    except Exception as e:
-        loguru.logger.error(traceback.format_exc())
-        send_a_message(traceback.format_exc())
-
-
-# DA telegram
-@client.on(events.ChatAction(chats=1002080008623))
-async def handler(event):
-    try:
-        # REDIS_URL = "redis://10.244.4.140:6379"
-        # REDIS_URL = "redis://10.244.4.58:6379"
-        # pool = aioredis.ConnectionPool.from_url(REDIS_URL, max_connections=10000)
-        # redis_client = aioredis.Redis(connection_pool=pool)
-
-        utc_time = datetime.utcnow().replace(tzinfo=pytz.utc)
-        formatted_utc_time = utc_time.strftime('%Y-%m-%d %H:%M:%S')
-        timestamp = int(utc_time.timestamp() * 1000)
 
         if event.user_joined or event.user_added:
             user = await event.get_user()
             if user:
                 loguru.logger.info(
-                    f'新成员加入DA: {user.id} - {user.first_name} {user.last_name if user.last_name else ""}')
+                    f'新成员加入 Alpha x: {user.id}')
 
                 information = {
                     "tg_id": user.id,
-                    "tg_first_name": user.first_name,
-                    "tg_last_name": user.last_name,
                     "tg_username": user.username,
-                    "tg_phone": user.phone,
-                    "create_time": timestamp,
-                    "update_time": timestamp,
-                    "time_at": formatted_utc_time
+                    "create_at": formatted_utc_time,
+                    "update_at": formatted_utc_time,
                 }
 
                 try:
                     async with async_session() as session:
-
-                        user_data = {
-                            "tg_code": 1
-                        }
-                        query1 = (
-                            update(t_users)
-                            .where(t_users.c.tg_id == str(user.id))
-                            .values(**user_data)
-                        )
-                        await session.execute(query1)
-                        await session.commit()
-
-                        query = insert(t_tg_users).prefix_with("IGNORE").values(information)
-                        await session.execute(query)
-                        await session.commit()
-
-                except Exception as e:
-                    loguru.logger.error(traceback.format_exc())
-                    send_a_message(traceback.format_exc())
-    except Exception as e:
-        loguru.logger.error(traceback.format_exc())
-        send_a_message(traceback.format_exc())
-
-
-# B2 telegram
-@client.on(events.ChatAction(chats=1001926465799))
-async def handler(event):
-    try:
-
-        utc_time = datetime.now(timezone.utc)
-        formatted_utc_time = utc_time.strftime('%Y-%m-%d %H:%M:%S')
-        timestamp = int(utc_time.timestamp() * 1000)
-
-        if event.user_joined or event.user_added:
-            user = await event.get_user()
-            if user:
-                loguru.logger.info(
-                    f'新成员加入B2: {user.id} - {user.first_name} {user.last_name if user.last_name else ""}')
-
-                information = {
-                    "tg_id": user.id,
-                    "tg_first_name": user.first_name,
-                    "tg_last_name": user.last_name,
-                    "tg_username": user.username,
-                    "tg_phone": user.phone,
-                    "create_time": timestamp,
-                    "update_time": timestamp,
-                    "time_at": formatted_utc_time
-                }
-
-                try:
-                    async with async_session() as session:
-
-                        user_data = {
-                            "tg_code": 1
-                        }
-                        query1 = (
-                            update(t_B2_user)
-                            .where(t_B2_user.c.tg_id == str(user.id))
-                            .values(**user_data)
-                        )
-                        await session.execute(query1)
-                        await session.commit()
-
-                        query = insert(B2_tg_info).prefix_with("IGNORE").values(information)
+                        query = insert(t_alphax_tg_user).prefix_with("IGNORE").values(information)
                         await session.execute(query)
                         await session.commit()
 
